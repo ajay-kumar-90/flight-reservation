e727ce924846616909034c5ca14a584e28e15d0d
ajay-kumar-90 <98318675+ajay-kumar-90@users.noreply.github.com>
2022-08-31 11:55:14 +0530

    Update FlightReservationApplication.java

--- src/main/java/com/ajaycodes/reservation/flight/FlightReservationApplication.java
+++ src/main/java/com/ajaycodes/reservation/flight/FlightReservationApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlightReservationApplication {
-	//dummy comment
     public static void main(String[] args) {
         SpringApplication.run(FlightReservationApplication.class, args);
     }
